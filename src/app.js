7ece61888c83ed4888600b689f4796065e236243
danidz96 <dani.d.zamorano@gmail.com>
2019-01-08 20:04:52 +0100

    add dropdown menu to select how to sort expenses

--- src/app.js
+++ src/app.js
@@ -13,11 +13,10 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 80, createdAt: 1 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 100, createdAt: 2 }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Restaurant', amount: 30, createdAt: 5 }));
+store.dispatch(addExpense({ description: 'Cinema', amount: 20, createdAt: 3 }));
+store.dispatch(addExpense({ description: 'New PC', amount: 1500, createdAt: 4 }));
 
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 4000);
 
 const state = store.getState();
 console.warn(state);
