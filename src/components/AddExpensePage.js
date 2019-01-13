c1a9ea53643cf1482dcf46ad48d71d21f836d416
danidz96 <dani.d.zamorano@gmail.com>
2019-01-13 18:43:02 +0100

    format with prettier

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
-  <div>
-    <h2>Add Expense</h2>
-    <ExpenseForm />
-  </div>
+	<div>
+		<h2>Add Expense</h2>
+		<ExpenseForm />
+	</div>
 );
 
 export default AddExpensePage;
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ id, dispatch, description, amount, createdAt }) => (
-  <div>
-    <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }));
-    }}>Delete</button>
-  </div>
-)
+	<div>
+		<h3>{description}</h3>
+		<p>
+			{amount} - {createdAt}
+		</p>
+		<button
+			onClick={() => {
+				dispatch(removeExpense({ id }));
+			}}
+		>
+			Delete
+		</button>
+	</div>
+);
 
 //Without destructuring
 
