18224d103ab0cc491b2bc37225a202ad1f5cf161
danidz96 <dani.d.zamorano@gmail.com>
2019-01-22 20:21:40 +0100

    add ExpenseForm tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,41 @@ test('should render expense form with expense data', () => {
 	const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
 	expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid form submisson', () => {
+	const wrapper = shallow(<ExpenseForm />);
+	expect(wrapper).toMatchSnapshot();
+	wrapper.find('form').simulate('submit', {
+		preventDefault: () => {}
+	});
+	expect(wrapper.state('error').length).toBeGreaterThan(0);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+	const value = 'New description';
+	const wrapper = shallow(<ExpenseForm />);
+	wrapper.find('input').at(0).simulate('change', {
+		target: { value }
+	});
+	expect(wrapper.state('description')).toBe(value);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should set note on input change', () => {
+	const value = 'New note';
+	const wrapper = shallow(<ExpenseForm />);
+	wrapper.find('textarea').simulate('change', {
+		target: { value }
+	});
+	expect(wrapper.state('note')).toBe(value);
+});
+
+test('should not set amount if input is invalid', () => {
+	const value = '22.2220';
+	const wrapper = shallow(<ExpenseForm />);
+	wrapper.find('input').at(1).simulate('change', {
+		target: { value }
+	});
+	expect(wrapper.state('amount')).toBe('');
+});
