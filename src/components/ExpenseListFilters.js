df2ec7f9b6b877bcdc90b937c3a6ed3ca9b56c3b
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-01-25 22:04:00 +0100

    add ExpenseListFilter tests

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,70 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
-class ExpenseListFilters extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			calendarFocused: null
-		};
-	}
-
-	onDatesChange = ({ startDate, endDate }) => {
-		this.props.dispatch(setStartDate(startDate));
-		this.props.dispatch(setEndDate(endDate));
-	};
-
-	onFocusChange = (calendarFocused) => {
-		this.setState({ calendarFocused });
-	};
-
-	render() {
-		return (
-			<div>
-				<input
-					type="text"
-					value={this.props.filters.text}
-					onChange={(e) => {
-						this.props.dispatch(setTextFilter(e.target.value));
-					}}
-				/>
-				<select
-					value={this.props.filters.sortBy}
-					onChange={(e) => {
-						if (e.target.value === 'date') {
-							this.props.dispatch(sortByDate());
-						} else if (e.target.value === 'amount') {
-							this.props.dispatch(sortByAmount());
-						}
-					}}
-				>
-					<option value="date">Date</option>
-					<option value="amount">Amount</option>
-				</select>
-				<DateRangePicker
-					startDate={this.props.filters.startDate}
-					startDateId="filter_start_date"
-					endDate={this.props.filters.endDate}
-					endDateId="filter_end_date"
-					onDatesChange={this.onDatesChange}
-					focusedInput={this.state.calendarFocused}
-					onFocusChange={this.onFocusChange}
-					isOutsideRange={() => false}
-					numberOfMonths={1}
-					showClearDates={true}
-				/>
-			</div>
-		);
-	}
-}
-
-const mapStateToProps = (state) => {
-	return {
-		filters: state.filters
-	};
+export class ExpenseListFilters extends React.Component {
+  state = {
+    calendarFocused: null
+  };
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
+  };
+  onFocusChange = (calendarFocused) => {
+    this.setState(() => ({ calendarFocused }));
+  }
+  onTextChange = (e) => {
+    this.props.setTextFilter(e.target.value);
+  };
+  onSortChange = (e) => {
+    if (e.target.value === 'date') {
+      this.props.sortByDate();
+    } else if (e.target.value === 'amount') {
+      this.props.sortByAmount();
+    }
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.props.filters.text}
+          onChange={this.onTextChange}
+        />
+        <select
+          value={this.props.filters.sortBy}
+          onChange={this.onSortChange}
+        >
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
+        <DateRangePicker
+          startDate={this.props.filters.startDate}
+		  onDatesChange={this.onDatesChange}
+		  startDateId="filter_start_date"
+		  endDate={this.props.filters.endDate}
+		  endDateId="filter_end_date"
+          focusedInput={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          showClearDates={true}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+      </div>
+    );
+  }
 };
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+const mapStateToProps = (state) => ({
+  filters: state.filters
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setTextFilter: (text) => dispatch(setTextFilter(text)),
+  sortByDate: () => dispatch(sortByDate()),
+  sortByAmount: () => dispatch(sortByAmount()),
+  setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+  setEndDate: (endDate) => dispatch(setEndDate(endDate))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
