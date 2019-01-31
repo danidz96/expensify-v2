d8fa59b603335bd41be3b9826324433ed9aca377
Dani <dani.d.zamorano@gmail.com>
2019-01-31 19:05:46 +0100

    setup dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,40 +8,43 @@ module.exports = (env) => {
 	return {
 		entry: './src/app.js',
 		output: {
-			path: path.join(__dirname, 'public'),
+			path: path.join(__dirname, 'public', 'dist'),
 			filename: 'bundle.js'
 		},
 		module: {
-			rules: [{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			}, {
-				test: /\.s?css$/,
-				use: CSSExtract.extract({
-					use: [{
-							loader: 'css-loader',
-							options: {
-								sourceMap: true
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.s?css$/,
+					use: CSSExtract.extract({
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true
+								}
 							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				})
-			}]
+						]
+					})
+				}
+			]
 		},
-		plugins: [
-			CSSExtract
-		],
+		plugins: [ CSSExtract ],
 		devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true
+			historyApiFallback: true,
+			publicPath: '/dist/'
 		}
 	};
 };
