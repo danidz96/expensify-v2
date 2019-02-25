787460d8b17ff62e412df3ccc0d5f99488d83ce4
Dani <dani.d.zamorano@gmail.com>
2019-02-25 19:12:04 +0100

    add list styles

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,28 +4,13 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-	<div>
-		<Link to={`/edit/${id}`}>
-			<h3>{description}</h3>
-		</Link>
-		<p>
-			{numeral(amount / 100).format('0,0.[00]$')}
-			-
-			{moment(createdAt).format('Do MMMM, YYYY')}
-		</p>
-	</div>
+	<Link className="list-item" to={`/edit/${id}`}>
+		<div>
+			<h3 className="list-item__title">{description}</h3>
+			<span className="list-item__sub-title">{moment(createdAt).format('Do MMMM, YYYY')}</span>
+		</div>
+		<h3 className="list-item__amount">{numeral(amount / 100).format('0,0.[00]$')}</h3>
+	</Link>
 );
-
-//Without destructuring
-
-// const ExpenseListItem = (props) => {
-//   const { description, amount, createdAt } = props.expense;
-//   return (
-//     <div>
-//       <h3>{description}</h3>
-//       <p>{amount} - {createdAt}</p>
-//     </div>
-//   )
-// }
 
 export default ExpenseListItem;
